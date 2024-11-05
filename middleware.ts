289d102f80cfdbdbc19835ff1c748eabbbdb0e7b
Derrick <derrickayaya@gmail.com>
2024-11-05 14:26:01 +0300

    Rollback to initial middleware

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,23 @@
-// export { auth as middleware } from "@/auth";
-
-// import { auth } from "@/auth";
+import { auth } from "@/auth";
 import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ["/middleware"];
 
 export default async function Middleware(request: NextRequest) {
+  const session = await auth();
+
   const isProtected = protectedRoutes.some((route) =>
     request.nextUrl.pathname.startsWith(route)
   );
 
-  // Instead of checking the session here, we'll redirect to an API route that will handle authentication
-  if (isProtected) {
-    const authUrl = new URL("/api/auth", request.url);
-    return NextResponse.redirect(authUrl);
+  if (!session && isProtected) {
+    const absoluteUrl = new URL("/", request.nextUrl.origin);
+    return NextResponse.redirect(absoluteUrl.toString());
   }
 
-  // If the route is not protected or the user is authenticated, allow the request
   return NextResponse.next();
 }
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-  runtime: "experimental-edge",
 };
