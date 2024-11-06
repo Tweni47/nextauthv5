1dc3808d51c92fd16dd424bc054f22828c142552
Derrick <derrickayaya@gmail.com>
2024-11-07 01:18:14 +0300

    edge runtime added to api

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -10,6 +10,7 @@ import { auth } from "@/auth";
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { Session } from "next-auth";
+export const runtime = "edge";
 
 interface AuthenticatedRequest extends NextRequest {
   auth: Session | null;
